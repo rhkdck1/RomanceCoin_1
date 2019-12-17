bcabad9e54103cb035c6984309833b8615f70336
rhkdck1 <40975867+rhkdck1@users.noreply.github.com>
2019-12-17 13:21:36 +0900

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -171,7 +171,7 @@ public:
         printf("consensus.hashGenesisBlockWork = %s\n", consensus.hashGenesisBlockWork.ToString().c_str());
         printf("genesis.hashMerkleRoot = %s\n", genesis.hashMerkleRoot.ToString().c_str());
         //assert(consensus.hashGenesisBlock == uint256S("0x14c03ecf20edc9887fb98bf34b53809f063fc491e73f588961f764fac88ecbae"));
-        assert(consensus.hashGenesisBlock == uint256S("0x000000006b387a60f06ffdf6dd5724e24b970df60775c2c42fc898868811f2d1"));
+        assert(consensus.hashGenesisBlock == uint256S("0x8ac7c879108003b45320732e652fdb3bdfa84dd97aab412e599332dadae4c719"));
         assert(consensus.hashGenesisBlockWork == uint256S("0x1294ab37ed4987a85c2b27ec96304e5629d222abcabe4705160f93dfca84a1f5"));
         assert(genesis.hashMerkleRoot == uint256S("0x0d1093709f96cd702036f506b09c3fbd18eb25cf57850dc25e2b5731604c2712"));
 
